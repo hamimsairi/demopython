aeaa315e37b8d8d26444292275fa2c35daf0f437
unknown <myadmin@WP-BPM-M4.modnet.mindef.my>
2018-10-30 10:53:31 +0800

    lalala

--- penganjur/urls.py
+++ penganjur/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='Penganjur Home'),
+    path('', views.home, name='penganjur_home'),
+    path('aktiviti/add/', views.penganjur_new, name='penganjur_add'),
 ]
--- penganjur/views.py
+++ penganjur/views.py
@@ -1,7 +1,52 @@
 from django.shortcuts import render
+from .models import Aktiviti 
+from .forms import AktivitiForm
 
 # Create your views here.
 
-#Penganjur HOME
+#Penganjur HOME######################################################################################
 def home(request):
+	listaktiviti = Aktiviti.objects.all()
+	print(request.GET['aktivitiid'])
+	#Print Data from Table
+	for v in listaktiviti:
+		print(v.tajuk,v.tempat,v.penceramah)
+
 	return render(request,'penganjur/home.html')
+
+# Tambah Penganjur######################################################################################
+def add_penganjur(request):
+
+	#add data stp kali request
+	akt= Aktiviti(tajuk='Tajuk Baru', tempat='Tak Kesah', penceramah='Paon', hadpeserta=55)
+	akt.save()
+
+	return render(request,'penganjur/home.html')
+
+#delete penganjur######################################################################################
+def delete_penganjur(request,pk):
+
+	#dptkan id aktivity n cari rekod
+	aktvt = get_object_or_404(Aktiviti,pk)
+
+	#confirm delete
+	aktvt.delete()
+	return render(request,'penganjur/home.html')	
+
+# Update Penganjur######################################################################################
+def update_penganjur(request,pk):
+
+	#add data stp kali request
+	akt=get_object_or_404(Aktiviti,pk)
+	akt= Aktiviti(tajuk='Tajuk Baru', tempat='Tak Kesah', penceramah='Paon', hadpeserta=55)
+	akt.save()
+
+	return render(request,'penganjur/home.html')	
+
+# Add Penganjur######################################################################################	
+def penganjur_new(request):
+	if request.method == "POST":
+		pass
+	else:
+		form = AktivitiForm()
+	return render(request,'penganjur/penganjur_add.html',{'form':form})	
